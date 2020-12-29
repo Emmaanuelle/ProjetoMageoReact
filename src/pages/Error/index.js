f74f4c2365d17acd58c62afb9c4aff701d0a48e8
Douglas Shibata <douglas.shibata11@gmail.com>
2020-12-29 15:48:18 -0300

    Arrumando pagina de erro

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -6,13 +6,12 @@ import imageErro from '../../images/error.jpg'
 function ErrorPage() {
     
     return (
-        <>
             <Container className='teste'>
                 <Link to='/'>Voltar para o Login</Link>
                 <h2 style={{ color: '#098348' }}>Página não Encontrada</h2>
-            <img src={imageErro} style={{position:'sticky'}} alt='erro' width='80%' height='10%' />
+            <img src={imageErro} style={{position:'relative'}} alt='erro' width='100%' height='100%' />
+                <p style={{ color: '#098348' }}>Por favor, Contate a Administradora</p>
             </Container>
-        </>
     )
 }
 
