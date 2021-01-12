cf9337c78664c3b7ecf65008abe1ea65271577d1
Emanuelle Ferreira <emanuelleferreiragod@gmail.com>
2021-01-12 12:59:05 -0300

    criando um grid, adicionando os dados do usuario na tela perfil por meio de um card, criando um card para ranking e pontuação.

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from '@material-ui/core'
+import { Container, Card, Grid, CardActionArea,CardContent, Typography } from '@material-ui/core'
 import Navbar from '../Navbar';
 import api from '../../services/api'
+
 
 
 function Perfil(props) {
@@ -20,13 +21,67 @@ function Perfil(props) {
         getItems()
     }, []);
 
+const datanascimento= new Date(items.dataNascimento);
+console.log(datanascimento)
+
     return (
         <>
             <Navbar />
             <Container>
                 <h2 style={{ margin: 20, color: '#098348' }}>Olá, {items.nome}</h2>
             </Container>
+            <Grid container spacing={3}> 
+            <Grid item xs={12}>
+                <Card>
+                    <CardActionArea>
+                       <CardContent>
+                           <Typography gutterBottom variant="h5" component="h2">
+                               Dados do usuario
+                           </Typography>
+                           <Typography gutterBottom variant="h5" component="h2">{items.nome} {items.sobrenome}
+                           </Typography>
+                           <Typography gutterBottom variant="h5" component="h2">{items.email}      
+                           </Typography>
+                           <Typography gutterBottom variant="h5" component="h2">{datanascimento.toDateString()}      
+                           </Typography>
+                           <Typography gutterBottom variant="h5" component="h2">{items.ano_escolar}      
+                           </Typography>
+                           <Typography gutterBottom variant="h5" component="h2">{items.escola}      
+                           </Typography>
+
+                       </CardContent>
+                    </CardActionArea>
+
+                </Card>
+                </Grid>
+               
+            <Grid item xs={12} sm={6}>
+                <Card>
+                    <CardActionArea>
+                       <CardContent>
+                           <Typography gutterBottom variant="h5" component="h2">
+                               Ranking
+                           </Typography>
+                       </CardContent>
+                    </CardActionArea>
+
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Card>
+                    <CardActionArea>
+                       <CardContent>
+                           <Typography gutterBottom variant="h5" component="h2">
+                             Pontuação
+                           </Typography>
+                       </CardContent>
+                    </CardActionArea>
+                    </Card>
+            </Grid>
+            </Grid>
+
         </>
+
     )
 }
 
