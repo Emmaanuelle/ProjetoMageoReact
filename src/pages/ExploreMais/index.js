fab11766418b450cba78a3bb23d1f646903a7d90
Emanuelle Ferreira <emanuelleferreiragod@gmail.com>
2021-01-16 11:21:40 -0300

    adicionie card na tela home com  as regras do jogo e  grid pra dividir o conteudo das fases. Na tela explore mais adicionei card com as questões sobre geometria plana com as alternativas.

--- src/pages/ExploreMais/index.js
+++ src/pages/ExploreMais/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Container, Card, Grid, CardMedia, CardActionArea, CardContent, Typography, CardActions, Button } from '@material-ui/core'
 import Navbar from '../Navbar';
-import logo from '../../images/imagen1.jpg'
+import logo from '../../images/imagen1.jpg';
+import Box from '@material-ui/core/Box';
 
 
 
 function ExploreMais() {
     var cardStyle = {
         display: 'block',
-        width: '30vw',
-        transitionDuration: '0.3s',
-        height: '35vw',
-        marginLeft: '50px'
+        width: '600px',
+        // transitionDuration: '0.3s',
+        height: '800px',
+         marginLeft: 0 
+       
     }
 
     return (
@@ -20,9 +22,140 @@ function ExploreMais() {
             <Container>
                 <h2 style={{ margin: 20, color: '#948324' }}>ExploreMais</h2>
             </Container>
+           {/*  <Box display="flex">
+           
+              <Box p={10} justifyContent="flex-end" >
+                
+                <p>
+                5. (Enem 2019) Em um condomínio, uma área pavimentada, que tem a forma de um círculo com diâmetro medindo 6 m, é cercada por grama. A administração do condomínio deseja ampliar essa área, mantendo seu formato circular, e aumentando, em 8 m, o diâmetro dessa região, mantendo o revestimento da parte já existente. O condomínio dispõe, em estoque, de material suficiente para pavimentar mais 100 m² de área.
+
+                    O síndico do condomínio irá avaliar se esse material disponível será suficiente para pavimentar a região a ser ampliada.
+
+                    Utilize 3 como aproximação para π.
+
+                    A conclusão correta a que o síndico deverá chegar, considerando a nova área a ser pavimentada, é a de que o material disponível em estoque
+                                        
+                </p>
+                <p>
+                <br/>
+                (A)será suficiente, pois a área da nova região a ser pavimentada mede 21 m2.
+                <br/>
+                (B) será suficiente, pois a área da nova região a ser pavimentada mede 24 m2.
+                (C)será suficiente, pois a área da nova região a ser pavimentada mede 48 m2.
+                (D)não será suficiente, pois a área da nova região a ser pavimentada mede 108 m2.
+                (E)não será suficiente, pois a área da nova região a ser pavimentada mede 120 m2.
+                </p>
+                <br/>
+                <p>
+                5. (Enem 2019) Em um condomínio, uma área pavimentada, que tem a forma de um círculo com diâmetro medindo 6 m, é cercada por grama. A administração do condomínio deseja ampliar essa área, mantendo seu formato circular, e aumentando, em 8 m, o diâmetro dessa região, mantendo o revestimento da parte já existente. O condomínio dispõe, em estoque, de material suficiente para pavimentar mais 100 m² de área.
+
+                    O síndico do condomínio irá avaliar se esse material disponível será suficiente para pavimentar a região a ser ampliada.
+
+                    Utilize 3 como aproximação para π.
+
+                    A conclusão correta a que o síndico deverá chegar, considerando a nova área a ser pavimentada, é a de que o material disponível em estoque
+                                        
+                </p>
+
+              </Box>
+
+            </Box> */}
             <Grid container spacing={3}>
+                <Grid item xs={10} sm={6}>
+                    <Card style={cardStyle}>
+                        <CardActionArea>
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="h2">
+                                    Questão Geometria Plana
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                (Enem 2019) Em um condomínio, uma área pavimentada, que tem a forma de um círculo com diâmetro medindo 6 m, é cercada por grama. A administração do condomínio deseja ampliar essa área, mantendo seu formato circular, e aumentando, em 8 m, o diâmetro dessa região, mantendo o revestimento da parte já existente. O condomínio dispõe, em estoque, de material suficiente para pavimentar mais 100 m² de área.
+
+                                O síndico do condomínio irá avaliar se esse material disponível será suficiente para pavimentar a região a ser ampliada.
+
+                                Utilize 3 como aproximação para π.
+
+                                A conclusão correta a que o síndico deverá chegar, considerando a nova área a ser pavimentada, é a de que o material disponível em estoque
+                                            
+                                </Typography>
+                                <Typography gutterBottom variant="h7" component="h2">
+                                    <p>
+                                    (A)será suficiente, pois a área da nova região a ser pavimentada mede 21 m2.
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    (B) será suficiente, pois a área da nova região a ser pavimentada mede 24 m2.
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    (C)será suficiente, pois a área da nova região a ser pavimentada mede 48 m2.
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    (D)não será suficiente, pois a área da nova região a ser pavimentada mede 108 m2.
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    (E)não será suficiente, pois a área da nova região a ser pavimentada mede 
+                                    </p>
+                                    <br/>
+                                
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+
+               
+
                 <Grid item xs={12} sm={6}>
                     <Card style={cardStyle}>
+                        <CardActionArea>
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="h2">
+                                    Questão Geometria Plana
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                (Enem 2019) Em um condomínio, uma área pavimentada, que tem a forma de um círculo com diâmetro medindo 6 m, é cercada por grama. A administração do condomínio deseja ampliar essa área, mantendo seu formato circular, e aumentando, em 8 m, o diâmetro dessa região, mantendo o revestimento da parte já existente. O condomínio dispõe, em estoque, de material suficiente para pavimentar mais 100 m² de área.
+
+                                O síndico do condomínio irá avaliar se esse material disponível será suficiente para pavimentar a região a ser ampliada.
+
+                                Utilize 3 como aproximação para π.
+
+                                A conclusão correta a que o síndico deverá chegar, considerando a nova área a ser pavimentada, é a de que o material disponível em estoque
+                                            
+                                </Typography>
+                                <Typography gutterBottom variant="h7" component="h2">
+                                    <p>
+                                    (A)será suficiente, pois a área da nova região a ser pavimentada mede 21 m2.
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    (B) será suficiente, pois a área da nova região a ser pavimentada mede 24 m2.
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    (C)será suficiente, pois a área da nova região a ser pavimentada mede 48 m2.
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    (D)não será suficiente, pois a área da nova região a ser pavimentada mede 108 m2.
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    (E)não será suficiente, pois a área da nova região a ser pavimentada mede 
+                                    </p>
+                                    <br/>
+                                
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+
+
+                <Grid item xs={12} sm={6}>
+                    {/* <Card style={cardStyle}>
                         <CardActionArea>
                             <CardMedia>
                                 <img src={logo} width="300px" />
@@ -43,11 +176,10 @@ function ExploreMais() {
                             </Button>
                         </CardActions>
 
-                    </Card>
+                    </Card> */}
                 </Grid>
-
                 <Grid item xs={12} sm={6}>
-                    <Card style={cardStyle}>
+                  {/*   <Card style={cardStyle}>
                         <CardActionArea>
                             <CardMedia>
                                 <img src={logo} width="300px" />
@@ -68,59 +200,7 @@ function ExploreMais() {
                             </Button>
                         </CardActions>
 
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia
-                                image="/src/images/icons/espadas.svg"
-                                title="Contemplative Reptile"
-
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    Video
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                    across all continents except Antarctica
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                Learn More
-                            </Button>
-                        </CardActions>
-
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia
-                                image="/src/images/icons/espadas.svg"
-                                title="Contemplative Reptile"
-
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    Video
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                    across all continents except Antarctica
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                Learn More
-                            </Button>
-                        </CardActions>
-
-                    </Card>
+                    </Card> */}
                 </Grid>
 
             </Grid>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,189 @@
 import React from 'react';
-import { Button, Container, Grid, ThemeProvider } from '@material-ui/core'
+import { Button, Container, Grid, ThemeProvider, Card, CardMedia, CardActionArea, CardContent, Typography, CardActions } from '@material-ui/core'
 import Navbar from '../Navbar';
 import { makeStyles } from '@material-ui/core/styles';
-import {theme} from '../../theme';
+import { theme } from '../../theme';
+import Box from '@material-ui/core/Box';
+import logo from '../../images/imagen2.jpg'
 const useStyles = makeStyles((theme) => ({
-    
-  
-    heroButtons: {
-      marginTop: theme.spacing(4),
-    },
-  }));
-const Home = (props) => {
-    const classes = useStyles();
 
-    return (
-        <>
-            <Navbar />
-            <ThemeProvider theme={theme}>
-            <Container>
-          
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary" href='/quiz'>
-                    Start Quiz
+
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  titulo: {
+    textAlign: "center",
+    marginTop: 60,
+    fontSize: 30,
+    color: "#769DE9",
+    fontWeight: "bold"
+  },
+  tituloregra:{ 
+  textAlign: "center",
+  marginTop: 60,
+  fontSize: 30,
+  color: "#769DE9",
+  fontWeight: "bold",
+  paddingTop:70
+  
+},
+
+  imagem:{
+    borderRadius:20,
+    height:350,
+    width:500,
+  },
+  gridmargen:{
+    marginLeft:"80px",
+    paddingLeft:"60px"
+  }
+  
+}));
+const Home = (props) => {
+  const classes = useStyles();
+  var cardStyle = {
+    display: 'block',
+    width: '300px',
+    // transitionDuration: '0.3s',
+    height: '300px',
+     marginLeft: 50,
+    
+   
+}
+
+  return (
+    <>
+      <Navbar />
+      <ThemeProvider theme={theme}>
+        <Container>
+
+         
+
+          <div className={classes.heroButtons}>
+            <Grid container spacing={2} justify="center">
+              <Grid item>
+                <Button variant="contained" color="primary" href='/quiz'>
+                  Start Quiz
                   </Button>
+              </Grid>
+            </Grid>
+          </div>
+          <div>
+            
+            <h1 className={classes.tituloregra}> Regras do Jogo</h1>
+            <Box display="flex">
+          <Box p={10} justifyContent="flex-start" >
+           {/*  <img src={logo} width="300px" className={classes.imagem} /> */}
+          </Box>
+          <Box p={10} justifyContent="flex-end" >
+              <ul>
+                <li> I - O jogador iniciará o jogo com 10 pontos. </li>
+                <li> II- Cada questão respondida corretamente corresponde a 10 pontos. </li>
+                <li> III-Se jogador acertar as 5 primeiras perguntas, recebe 50 pontos + o dobro de pontos ganho e vence o 1º desafio da fase. </li>
+                <li> IV- Se jogador vencer as 5 próximas perguntas, recebe 50 pontos + o dobro de pontos ganho e vence o 2º desafio da fase. </li>
+                <li> V-  Ao vencer os 2 desafios o jogador conquista  o emblema da fase. </li>
+            
+              </ul>
+          </Box>
+          
+        </Box>
+        <Grid container spacing={5} className={classes.gridmargen}>
+              <Grid item xs={12} sm={4}>
+                     <Card style={cardStyle}>
+                       <CardActionArea>
+                            <CardMedia>
+                                <img src={logo} width="300px" />
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                  Emblema
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                  Pontuação
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card> 
                 </Grid>
+              
+                <Grid item xs={12} sm={6}>
+                     <Card style={cardStyle}>
+                       <CardActionArea>
+                            <CardMedia>
+                                <img src={logo} width="300px" />
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                  Emblema
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                  Pontuação
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card> 
                 </Grid>
-                </div>
-            </Container>
-            </ThemeProvider>
-        </>
-    );
+            </Grid>
+            
+      
+            <h1 className={classes.titulo}> Fases do Quiz</h1>
+            <Box display="flex">
+
+              <Box p={10} justifyContent="flex-start" >
+                <img src={logo} width="300px" />
+              </Box>
+              <Box p={10} justifyContent="flex-end" >
+                <h1> Triângulo</h1>
+                <p> pré-elaborados. O formulário possui em sua estrutura opções de perguntas os em cada pergunta desenvolvida.</p>
+              </Box>
+
+            </Box>
+            <Box display="flex">
+
+              <Box p={10} justifyContent="flex-start" >
+
+                <h1> Quadrado</h1>
+                <p> pré-elaborados. O formulário possui em sua estrutura opções de perguntas os em cada pergunta desenvolvida.</p>
+              </Box>
+              <Box p={6} justifyContent="flex-end" >
+                <img src={logo} width="300px" />
+              </Box>
+
+            </Box>
+            <Box display="flex">
+
+              <Box p={10} justifyContent="flex-start" >
+                <img src={logo} width="300px" />
+              </Box>
+              <Box p={10} justifyContent="flex-end" >
+                <h1> Quadrado</h1>
+                <p> pré-elaborados. O formulário possui em sua estrutura opções de perguntas os em cada pergunta desenvolvida.</p>
+              </Box>
+
+            </Box>
+            <Box display="flex">
+
+              <Box p={10} justifyContent="flex-start" >
+
+                <h1> Círculo</h1>
+                <p> pré-elaborados. O formulário possui em sua estrutura opções de perguntas os em cada pergunta desenvolvida.</p>
+              </Box>
+              <Box p={6} justifyContent="flex-end" >
+                <img src={logo} width="300px" />
+              </Box>
+
+            </Box>
+
+
+
+
+
+
+          </div>
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default Home;
