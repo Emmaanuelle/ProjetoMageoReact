8f51a95ddde66b3d166e634121cc0e057079891e
Emanuelle Ferreira <emanuelleferreiragod@gmail.com>
2021-01-28 18:06:36 -0300

    mudando a p para produção

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333' //process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL
+  //http://127.0.0.1:3333'
 });
 
 api.interceptors.request.use(async config => {
