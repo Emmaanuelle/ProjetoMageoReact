ab415e3ceca8e6e3e8de4aea22e39902d4019f6c
Douglas Shibata <douglas.shibata11@gmail.com>
2021-01-28 19:47:54 -0300

    Adicionando https

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import "./styles/styles.css";
 import { theme } from './theme';
+import HttpsRedirect from 'react-https-redirect';
 
 ReactDOM.render(
+    <HttpsRedirect>
     <React.StrictMode>
         <ThemeProvider theme={theme}>
         <App />
         </ThemeProvider>
     </React.StrictMode>
-
+    </HttpsRedirect>
     , document.getElementById('root')
 
 );
