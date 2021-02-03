7e0c7dab45b594461813173c7d57ad0ed8cc0737
Douglas Shibata <douglas.shibata11@gmail.com>
2021-02-03 09:16:58 -0300

    Listando e editando questao

---
+++ src/pages/ListarQuestao/EditarQuestoes.js
@@ -0,0 +1,216 @@
+import React, { useState } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
+import { MenuItem} from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import api from '../../services/api';
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+
+export default function EditarQuestoes(props) {
+  const questoes = props.questoes
+  console.log(questoes);
+  const [pergunta, setPergunta] = useState(questoes.pergunta);
+  const [resposta, setResposta] = useState(questoes.resposta);
+  const [alternativa, setAlternativa] = useState(questoes.alternativa);
+  const [dica, setDica] = useState(questoes.dica);
+  const [video, setVideo] = useState(questoes.video);
+  const [nivel, setNivel] = useState(questoes.nivel);
+  const[fase,setFase] = useState(questoes.fase);
+  const [error, setError] = useState("");
+  const [carregar, setCarregando] = useState(false);
+
+  const [open, setOpen] = React.useState(false);
+
+   // const history = useHistory();
+   async function editarQuestoes(e) {
+
+    e.preventDefault();
+
+    const data = { pergunta, resposta, dica, alternativa, video, fase, nivel};
+    try {
+      setCarregando(true)
+      await api.put(`/questao/${questoes.id}`, data)
+      alert("Questão Atualizada com Sucesso")
+      setTimeout(()=>{window.location.reload()},300)  //recarregar a pagina
+      // history.push('/quiz')
+    } catch (error) {
+      console.log(error.response.data)
+      if(error.response.data.error.message === 'E_INVALID_JWT_TOKEN: invalid signature'){
+        setError("Você não tem permissão para cadastrar uma questão")
+
+      }else{
+        setError("Houve um erro ao cadastrar a questão, tente novamente")
+
+      }
+    }
+
+  }
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        Editar
+      </Button>
+      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+         Editar Questoes
+        </DialogTitle>
+        <DialogContent dividers>
+      {error&&<p>{error}</p>}
+      {carregar&&!error?<p>Carregando</p>:<></>}
+      <form onSubmit={editarQuestoes}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  name="pergunta"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="pergunta"
+                  label="Pergunta"
+                  autoFocus
+                  value={pergunta}
+                  onChange={e => setPergunta(e.target.value)}
+                />
+              </Grid>
+       
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="resposta"
+                  label="Resposta"
+                  name="Resposta"
+                  value={resposta}
+                  onChange={e => setResposta(e.target.value)}
+                />
+              </Grid>
+     
+              
+              <Grid item xs={12} >
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="dica"
+                  label="dica"
+                  autoComplete="dica"
+                  value={dica}
+                  onChange={e => setDica(e.target.value)}
+                />
+              </Grid>
+ 
+              <Grid item xs={12} >
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="alternativas"
+                  label="alternativas"
+                  placeholder="alternativas incorretas, separada por virgula"
+                  autoComplete="alternativas"
+                  value={alternativa}
+                  onChange={e => setAlternativa(e.target.value)}
+                />
+              </Grid>
+           
+              <Grid item xs={12} >
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="video"
+                  label="Link para o youtube"
+                  name="video"
+                  value={video}
+                  onChange={e => setVideo(e.target.value)}
+                />
+              </Grid>
+          
+              <Grid item xs={12}>
+                <TextField id="fase" value={fase} onChange={e => setFase(e.target.value)} variant="outlined" select fullWidth label="Selecione o Tipo da Questão">
+               {/*  <InputLabel id="demo-simple-select-helper-label">Selecione o  Tipo da Questão</InputLabel> */}
+                  <MenuItem value={"quadrado"}>Quadrado</MenuItem>
+                  <MenuItem value={"retangulo"}>Retângulo</MenuItem>
+                  <MenuItem value={"triangulo"}>Triângulo</MenuItem>
+                  <MenuItem value={"circulo"}> Círculo</MenuItem>
+                </TextField >
+              </Grid>
+              <Grid item xs={12}>
+                <TextField id="nivel" value={fase} onChange={e => setNivel(e.target.value)} variant="outlined" select fullWidth label="Selecione o Nível da Questão">
+               {/*  <InputLabel id="demo-simple-select-helper-label">Selecione o  Tipo da Questão</InputLabel> */}
+                  <MenuItem value={"facil"}> Fácil</MenuItem>
+                  <MenuItem value={"medio"}>Médio</MenuItem>
+                  <MenuItem value={"dificil"}>Dificil</MenuItem>
+                  <MenuItem value={"desafioquestao"}> Desafio Questão</MenuItem>
+                </TextField >
+              </Grid>
+
+            </Grid>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Salvar
+          </Button>
+          </form>
+
+         
+         
+        </DialogContent>
+
+      </Dialog>
+    </div>
+  );
+}
--- src/pages/ListarQuestao/index.js
+++ src/pages/ListarQuestao/index.js
@@ -1,20 +1,11 @@
 import React, { useState,useEffect} from "react";
 import api from "../../services/api"
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import ReactLoading from 'react-loading';
 import Navbar from "../NavbarAdministrador";
-import Tooltip from '@material-ui/core/Tooltip';
-import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined';
-//import { useHistory } from "react-router-dom";
-import { MenuItem} from "@material-ui/core";
-
-
+import EditarQuestoes from "./EditarQuestoes";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -23,49 +14,14 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: "#509312",
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  /* tamanhoselect:{
-    minWidth:320,
-    //backgroundColor:"#509312"
-  }, */
-  
 }));
 
 
-
-const DicaParaInput = withStyles((theme) => ({
-  tooltip: {
-    backgroundColor: '#f5f5f9',
-    color: 'rgba(0, 0, 0, 0.87)',
-    maxWidth: 220,
-    fontSize: theme.typography.pxToRem(12),
-    border: '1px solid #dadde9',
-  },
-}))(Tooltip);
 
 
 export default function ListarQuestao() {
   const classes = useStyles();
 
-  const [carregar, setCarregando] = useState(false);
-  const [pergunta, setPergunta] = useState("");
-  const [resposta, setResposta] = useState("");
-  const [alternativa, setAlternativa] = useState([]);
-  const [dica, setDica] = useState("");
-  const [video, setVideo] = useState("");
-  const [nivel, setNivel] = useState("");
-  const[fase,setFase] = useState("");
-  const [error, setError] = useState("");
 
   const [questoes, setQuestoes] = useState([])
     useEffect(() => {
@@ -82,30 +38,7 @@ export default function ListarQuestao() {
         }
         getQuestoes()
     }, []);
-  // const history = useHistory();
-  async function cadastrarQuestoes(e) {
-
-    e.preventDefault();
-
-    const data = { pergunta, resposta, dica, alternativa, video, fase, nivel};
-    try {
-      setCarregando(true)
-      await api.post("/questao", data)
-      alert("Questão cadastrada com Sucesso")
-      setTimeout(()=>{window.location.reload()},300)  //recarregar a pagina
-      // history.push('/quiz')
-    } catch (error) {
-      console.log(error.response.data)
-      if(error.response.data.error.message === 'E_INVALID_JWT_TOKEN: invalid signature'){
-        setError("Você não tem permissão para cadastrar uma questão")
-
-      }else{
-        setError("Houve um erro ao cadastrar a questão, tente novamente")
-
-      }
-    }
-
-  }
+ 
   return (
     <>
       <Navbar />
@@ -115,171 +48,15 @@ export default function ListarQuestao() {
           <Typography component="h1" variant="h5">
             Listar Questão
         </Typography>
-          {carregar && !error ? <ReactLoading className='loading' type={"bubbles"} color={'orange'} height={'20%'} width={'40%'} /> : <></>}
             {questoes.length > 0 ? questoes.map((valor,indice)=>{
               return(
-                <div>
+                <div key={valor.id}>
                   <p>{valor.pergunta}</p><hr/>
-
+                <EditarQuestoes questoes={valor}/>
                 </div>
               )
             }):<p>Não tem perguntas cadastrada no momento!</p>}
-          <form className={classes.form} onSubmit={cadastrarQuestoes}>
-            {error && <p className='error'>{error}</p>}
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={11}>
-                <TextField
-                  name="pergunta"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="pergunta"
-                  label="Pergunta"
-                  autoFocus
-                  value={pergunta}
-                  onChange={e => setPergunta(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={1}>
-                <DicaParaInput
-                  title={
-                    <React.Fragment>
-                      <Typography color="secondary">Qual pergunta colocar ?</Typography>
-                        Explicação da pergunta
-                     </React.Fragment>
-                  }
-                >
-                  <HelpOutlineOutlinedIcon />
-                </DicaParaInput>
-              </Grid>
-              <Grid item xs={12} sm={11}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="resposta"
-                  label="Resposta"
-                  name="Resposta"
-                  value={resposta}
-                  onChange={e => setResposta(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={1}>
-                <DicaParaInput
-                  title={
-                    <React.Fragment>
-                      <Typography color="secondary">Resposta ?</Typography>
-                        Explicação do formato da Resposta
-                     </React.Fragment>
-                  }
-                >
-                  <HelpOutlineOutlinedIcon />
-                </DicaParaInput>
-              </Grid>
-              
-              <Grid item xs={12} sm={11} >
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="dica"
-                  label="dica"
-                  autoComplete="dica"
-                  value={dica}
-                  onChange={e => setDica(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={1}>
-                <DicaParaInput
-                  title={
-                    <React.Fragment>
-                      <Typography color="secondary">Dica ?</Typography>
-                        Explicação do formato da dica
-                     </React.Fragment>
-                  }
-                >
-                  <HelpOutlineOutlinedIcon />
-                </DicaParaInput>
-              </Grid>
-              <Grid item xs={12} sm={11} >
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="alternativas"
-                  label="alternativas"
-                  placeholder="alternativas incorretas, separada por virgula"
-                  autoComplete="alternativas"
-                  value={alternativa}
-                  onChange={e => setAlternativa(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={1}>
-                <DicaParaInput
-                  title={
-                    <React.Fragment>
-                      <Typography color="secondary">Alternativas ?</Typography>
-                        Explicação do formato das Alternativas
-                     </React.Fragment>
-                  }
-                >
-                  <HelpOutlineOutlinedIcon />
-                </DicaParaInput>
-              </Grid>
-              <Grid item xs={12} sm={11} >
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  id="video"
-                  label="Link para o youtube"
-                  name="video"
-                  value={video}
-                  onChange={e => setVideo(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={1}>
-                <DicaParaInput
-                  title={
-                    <React.Fragment>
-                      <Typography color="secondary">Youtube ?</Typography>
-                        Explicação do formato do youtube
-                     </React.Fragment>
-                  }
-                >
-                  <HelpOutlineOutlinedIcon />
-                </DicaParaInput>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField id="fase" value={fase} onChange={e => setFase(e.target.value)} variant="outlined" select fullWidth label="Selecione o Tipo da Questão">
-               {/*  <InputLabel id="demo-simple-select-helper-label">Selecione o  Tipo da Questão</InputLabel> */}
-                  <MenuItem value={"quadrado"}>Quadrado</MenuItem>
-                  <MenuItem value={"retangulo"}>Retângulo</MenuItem>
-                  <MenuItem value={"triangulo"}>Triângulo</MenuItem>
-                  <MenuItem value={"circulo"}> Círculo</MenuItem>
-                </TextField >
-              </Grid>
-              <Grid item xs={12}>
-                <TextField id="nivel" value={fase} onChange={e => setNivel(e.target.value)} variant="outlined" select fullWidth label="Selecione o Nível da Questão">
-               {/*  <InputLabel id="demo-simple-select-helper-label">Selecione o  Tipo da Questão</InputLabel> */}
-                  <MenuItem value={"facil"}> Fácil</MenuItem>
-                  <MenuItem value={"medio"}>Médio</MenuItem>
-                  <MenuItem value={"dificil"}>Dificil</MenuItem>
-                  <MenuItem value={"desafioquestao"}> Desafio Questão</MenuItem>
-                </TextField >
-              </Grid>
-
-            </Grid>
-
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Cadastrar
-          </Button>
-          </form>
+      
         </div>
       </Container>
     </>
