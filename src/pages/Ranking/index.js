253bfcbe6a0603fc4f469ea1a26054377ec1861a
Emanuelle Ferreira <emanuelleferreiragod@gmail.com>
2021-02-12 13:55:54 -0300

    adicionando um titulo para escolha das fases

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -5,6 +5,8 @@ import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableFooter from '@material-ui/core/TableFooter';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
@@ -40,7 +42,12 @@ const StyledTableCell = withStyles((theme) => ({
 
 function Ranking() {
     const classes = useStyles();
-    const [pontuacao, setPontuacao] = useState([])
+    const [pontuacao, setPontuacao] = useState([]);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+
+
     useEffect(() => {
         const getPontuacao = async () => {
             try {
@@ -62,6 +69,7 @@ function Ranking() {
         getPontuacao()
     }, []);
 
+    var count=1;
     return (
         <>
             <Navbar />
@@ -71,8 +79,11 @@ function Ranking() {
       <Table className={classes.table} aria-label="customized table">
         <TableHead>
           <TableRow>
+          <StyledTableCell>Colocação</StyledTableCell>
             <StyledTableCell>Nome</StyledTableCell>
             <StyledTableCell align="right">Pontuação</StyledTableCell>
+            
+
          
             
           </TableRow>
@@ -81,6 +92,9 @@ function Ranking() {
           {pontuacao.map((infopontuacao) => (
             <StyledTableRow key={infopontuacao.id}>
               <StyledTableCell component="th" scope="row">
+                {count++} º
+              </StyledTableCell>
+              <StyledTableCell component="th" scope="row">
                 {infopontuacao.nome}
               </StyledTableCell>
               <StyledTableCell align="right">{infopontuacao.pontos}</StyledTableCell>
@@ -88,6 +102,24 @@ function Ranking() {
             </StyledTableRow>
           ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: { 'aria-label': 'rows per page' },
+                native: true,
+              }}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
             </Container>
