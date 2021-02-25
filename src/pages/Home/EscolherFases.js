2010fb857191b7a91d78714e4fd0af61c8723df6
Emanuelle Ferreira <emanuelleferreiragod@gmail.com>
2021-02-25 18:57:18 -0300

    adiconando imagens e alterando o arquivo escolher fases.

--- src/pages/Home/EscolherFases.js
+++ src/pages/Home/EscolherFases.js
@@ -4,10 +4,10 @@ import Navbar from '../Navbar';
 import { CardContent, Container, Typography, Card, Button, Grid,Paper} from '@material-ui/core';
 import circle from '../../images/icons/circle.svg';
 import  triangles from '../../images/icons/triangles.svg';
-import reta  from '../../images/icons/reta.svg';
-import Retang from '../../images/icons/Retang.svg';
-import fig3 from '../../images/icons/fig3.svg';
-import fig4 from '../../images/icons/fig4.svg';
+import reta  from '../../images/icons/Rectanglee.svg';
+import Retang from '../../images/icons/verde.svg';
+import fig3 from '../../images/icons/Pol.svg';
+import fig4 from '../../images/icons/Ell.svg';
 
 
 
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme)=>({
     conteudo:{
         width:"70%",
         height:"60%",   
-        backgroundColor:"#11CCBB",
+        backgroundColor:"#733AC9",
         color:"white",
         fontWeight:"Bold",
         textAlign:"center",
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme)=>({
     conteudo1:{
         width:"70%",
         height:"60%", 
-        backgroundColor:"#11CCBB",
+        backgroundColor:"#733AC9",
         color:"white",
         fontWeight:"Bold",
         textAlign:"center",
@@ -33,7 +33,7 @@ const useStyles = makeStyles((theme)=>({
     conteudo2:{
         width:"70%",
         height:"60%",   
-        backgroundColor:"#11CCBB",
+        backgroundColor:"#733AC9",
         color:"white",
         fontWeight:"Bold",
         textAlign:"center",
@@ -42,7 +42,7 @@ const useStyles = makeStyles((theme)=>({
     conteudo3:{
         width:"70%",
         height:"60%",  
-        backgroundColor:"#11CCBB",
+        backgroundColor:"#733AC9",
         color:"white",
         fontWeight:"Bold",
         textAlign:"center",
@@ -50,18 +50,37 @@ const useStyles = makeStyles((theme)=>({
 
     },
     fase:{
-        margin:"auto",
         fontWeight:"Bold",
         marginLeft:"50px",
         fontFamily:"'Poppins', sans-serif;",
+        fontSize:"1.25rem",
+        Color:"#FFBB0E",
+        
         
     },
     fase1:{
-        margin:"auto",
         fontWeight:"Bold",
         marginLeft:"50px",
         fontFamily:"'Poppins', sans-serif;",
+        fontSize:"1.25rem",
         Color:"#FFBB0E",
+        
+    },
+    fase2:{
+        fontWeight:"Bold",
+        marginLeft:"50px",
+        fontFamily:"'Poppins', sans-serif;",
+        fontSize:"1.25rem",
+        Color:"#FFBB0E",
+        
+    },
+    fase3:{
+        fontWeight:"Bold",
+        marginLeft:"50px",
+        fontFamily:"'Poppins', sans-serif;",
+        fontSize:"1.25rem",
+        Color:"#FFBB0E",
+        
     },
     paper:{
         marginBottom:"20px",
@@ -73,8 +92,9 @@ const useStyles = makeStyles((theme)=>({
         fontSize:"20px",
         marginBottom:"25px",
         fontFamily:"'Poppins', sans-serif;",
+        color:"#9572F8"
     },
-
+/* 
     fundo:{
         background:"#E5E7EB",
     },
@@ -89,7 +109,7 @@ const useStyles = makeStyles((theme)=>({
     
     fundo3:{
         backgroundColor:"#E5E7EB",
-    },
+    }, */
     linha:{
         marginBottom:"30px"
     },
@@ -97,24 +117,25 @@ const useStyles = makeStyles((theme)=>({
         width:"150px",
         height:"120px",
         marginLeft:"50px",
-        marginTop:"10%"
+        marginTop:"15%"
     },
     imagem1:{
         width:"150px",
         height:"150px",
         marginLeft:"50px",
-        marginTop:"10%"
+        marginTop:"2%"
     },
     imagem2:{
         width:"150px",
         height:"150px",
         marginLeft:"50px",
-        marginTop:"10%"
+        marginTop:"2%"
     },
     imagem3:{
         width:"150px",
         height:"150px",
-        marginLeft:"50px"
+        marginLeft:"50px",
+        marginTop:"2%"
     },
     
   }));
@@ -136,16 +157,16 @@ return(
                     MaGeo Fases
                 </Typography>
                 <Typography className={classes.titulo1}>
-                    Ganhe pontos! Testando os seus conhecimentos em Geoemtria PLana.
+                    Ganhe pontos! Testando os seus conhecimentos em Geometria Plana.
                 </Typography>
                 <Typography style={{ marginBottom: 20, color: '#6A6A6A', fontSize: "190%", textAlign: "center", fontWeight:600, marginTop:"3%"}}>
-                    <hr/> Fases 
+                  
                  {/*    
                     style={{backgroundColor:"#0DD28B", content: "Fases",displayInline:"block", }} */}
                 </Typography>
            
                 <Grid container spacing={7} className={classes.linha} >
-                    
+                
                 <Grid   item xs={12} spacing={9} sm={3}>
                 <Paper className={classes.paper}> 
                 <Card className={classes.fundo}>
@@ -185,7 +206,7 @@ return(
                 <Card className={classes.fundo2}>
                 <CardContent >
                     {/* style={{background: `url(${triangles})`, backgroundSize:"cover", }} */}
-                <span className={classes.fase}>
+                <span className={classes.fase2}>
                 Fase Triângulo
                 <img src={fig3} alt='Logo do Projeto Mageo'  className={classes.imagem2}/>
                 <Button href="/faseTriangulo" className={classes.conteudo2} > Jogar</Button>  
@@ -200,7 +221,7 @@ return(
                 <Paper className={classes.paper}>
                 <Card className={classes.fundo3}>
                 <CardContent>
-                <span className={classes.fase}>
+                <span className={classes.fase3}>
                 Fase Circulo
                 <img src={fig4} alt='Logo do Projeto Mageo'  className={classes.imagem3}/> 
                 <Button href="/faseCirculo" className={classes.conteudo3}>Jogar</Button>
